25823f096fde95e293a9f1ffc132dc769ab022a3
ozangunalp <ozangunalp@gmail.com>
2013-04-25 11:31:14 +0200

    this one should pass, but should write better tests about osgi...

--- everest-osgi/src/test/java/org/apache/felix/ipojo/everest/osgi/impl/TestOsgiRootResource.java
+++ everest-osgi/src/test/java/org/apache/felix/ipojo/everest/osgi/impl/TestOsgiRootResource.java
@@ -6,9 +6,14 @@ import org.apache.felix.ipojo.everest.services.IllegalActionOnResourceException;
 import org.apache.felix.ipojo.everest.services.ResourceNotFoundException;
 import org.junit.Before;
 import org.junit.Test;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.wiring.FrameworkWiring;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Test the iPOJO m_everest resource.
@@ -23,11 +28,18 @@ public class TestOsgiRootResource {
         m_everest = new Everest();
 
         // Create a fake bundle context.
+        Bundle zero = mock(Bundle.class);
         BundleContext context = mock(BundleContext.class);
+
+        when(zero.getBundleContext()).thenReturn(context);
+        when(context.getBundle(0)).thenReturn(zero);
+        when(context.getProperty(anyString())).thenReturn("Some Property");
+        when(zero.adapt(any(Class.class))).thenReturn(mock(FrameworkWiring.class));
 
         m_osgi = new OsgiRootResource(context);
         m_everest.bindRootResource(m_osgi);
     }
+
 
     @Test
     public void testEmpty() throws ResourceNotFoundException, IllegalActionOnResourceException {
