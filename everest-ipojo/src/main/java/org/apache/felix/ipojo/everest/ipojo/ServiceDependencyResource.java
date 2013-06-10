4b444a94d6bae34b58a94f65c538d1d4bb018c25
Pierre Bourret <Pierre.Bourret@imag.fr>
2013-06-10 11:26:12 +0200

    everest-ipojo: service dependency resource got metadata + relations

--- everest-ipojo/src/main/java/org/apache/felix/ipojo/everest/ipojo/ServiceDependencyResource.java
+++ everest-ipojo/src/main/java/org/apache/felix/ipojo/everest/ipojo/ServiceDependencyResource.java
@@ -1,36 +1,171 @@
 package org.apache.felix.ipojo.everest.ipojo;
 
-import org.apache.felix.ipojo.ComponentInstance;
-import org.apache.felix.ipojo.Factory;
-import org.apache.felix.ipojo.InstanceStateListener;
-import org.apache.felix.ipojo.architecture.Architecture;
 import org.apache.felix.ipojo.everest.core.Everest;
 import org.apache.felix.ipojo.everest.impl.DefaultReadOnlyResource;
 import org.apache.felix.ipojo.everest.impl.DefaultRelation;
 import org.apache.felix.ipojo.everest.impl.ImmutableResourceMetadata;
 import org.apache.felix.ipojo.everest.services.*;
+import org.apache.felix.ipojo.handlers.dependency.Dependency;
 import org.apache.felix.ipojo.handlers.dependency.DependencyDescription;
-import org.apache.felix.ipojo.handlers.dependency.DependencyHandlerDescription;
+import org.apache.felix.ipojo.util.DependencyModel;
+import org.apache.felix.ipojo.util.DependencyStateListener;
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceReference;
 
 import java.lang.ref.WeakReference;
-import java.util.Hashtable;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-import static org.apache.felix.ipojo.everest.ipojo.IpojoRootResource.*;
+import static org.apache.felix.ipojo.everest.ipojo.IpojoRootResource.PATH_TO_OSGI_SERVICES;
+import static org.apache.felix.ipojo.util.DependencyModel.*;
 
 /**
- * '/ipojo/instance/$name/dependency/$is' resource.
+ * '/ipojo/instance/$name/dependency/$id' resource.
  */
-public class ServiceDependencyResource extends DefaultReadOnlyResource {
+// TODO Register as a dependency listener as soon as iPOJO permits it.
+public class ServiceDependencyResource extends DefaultReadOnlyResource implements DependencyStateListener {
 
-    public ServiceDependencyResource(InstanceResource instance, DependencyDescription dependency) {
-        super(instance.getDependencies().getPath().addElements(dependency.getId()));
+    private WeakReference<DependencyDescription> m_description;
+
+    public ServiceDependencyResource(InstanceResource instance, DependencyDescription description) {
+        super(instance.getDependencies().getPath().addElements(description.getId()),
+                new ImmutableResourceMetadata.Builder()
+                        .set("id", description.getId())
+                        .set("specification", description.getSpecification()) // May become mutable is future iPOJO releases?
+                        .set("isNullable", description.supportsNullable())
+                        .set("isProxy", description.isProxy())
+                        .set("defaultImplementation", description.getDefaultImplementation())
+                                // All other attributes are considered dynamic
+                        .build());
+        m_description = new WeakReference<DependencyDescription>(description);
     }
 
-    // TODO implement dependency state listener
+    @Override
+    public ResourceMetadata getMetadata() {
+        ResourceMetadata m = super.getMetadata();
+        DependencyDescription d = m_description.get();
+        if (d == null) {
+            return m;
+        } else {
+            //@SuppressWarnings("unchecked")
+            List refs = d.getServiceReferences();
+            List used = d.getUsedServices();
+            return new ImmutableResourceMetadata.Builder()
+                    .set("state", stateToString(d.getState()))
+                    .set("filter", d.getFilter())
+                    .set("policy", policyToString(d.getPolicy()))
+                    .set("comparator", d.getComparator())
+                    .set("isAggregate", d.isMultiple())
+                    .set("isOptional", d.isOptional())
+                    .set("isFrozen", d.isFrozen())
+                    .build();
+        }
+    }
+
+    @Override
+    public List<Relation> getRelations() {
+        List<Relation> r = super.getRelations();
+        DependencyDescription d = m_description.get();
+        if (d == null) {
+            return r;
+        } else {
+            r = new ArrayList<Relation>(r);
+            @SuppressWarnings("unchecked")
+            List<ServiceReference<?>> matching = d.getServiceReferences();
+            if (matching != null) {
+                for(ServiceReference<?> s : matching) {
+                    String id = String.valueOf(s.getProperty(Constants.SERVICE_ID));
+                    r.add(new DefaultRelation(
+                            PATH_TO_OSGI_SERVICES.addElements(id),
+                            Action.READ,
+                            String.format("matchingService[%s]", id),
+                            String.format("Matching service with id '%s'", id)));
+                }
+            }
+            @SuppressWarnings("unchecked")
+            List<ServiceReference<?>> used = d.getUsedServices();
+            for(ServiceReference<?> s : used) {
+                String id = String.valueOf(s.getProperty(Constants.SERVICE_ID));
+                r.add(new DefaultRelation(
+                        PATH_TO_OSGI_SERVICES.addElements(id),
+                        Action.READ,
+                        String.format("usedService[%s]", id),
+                        String.format("Used service with id '%s'", id)));
+            }
+            return Collections.unmodifiableList(r);
+        }
+    }
+
     @Override
     public boolean isObservable() {
         return true;
+    }
+
+    @Override
+    public <A> A adaptTo(Class<A> clazz) {
+        if (clazz == DependencyModel.class) {
+            return clazz.cast(getDependency(m_description.get()));
+        } else if (clazz == DependencyDescription.class) {
+            return clazz.cast(m_description.get());
+        } else {
+            return super.adaptTo(clazz);
+        }
+    }
+
+    public void validate(DependencyModel dependency) {
+        // Fire UPDATED event
+        Everest.postResource(ResourceEvent.UPDATED, this);
+    }
+
+    public void invalidate(DependencyModel dependency) {
+        // Fire UPDATED event
+        Everest.postResource(ResourceEvent.UPDATED, this);
+    }
+
+    private static String policyToString(int policy) {
+        switch (policy) {
+            case DYNAMIC_BINDING_POLICY:
+                return "DYNAMIC_BINDING";
+            case STATIC_BINDING_POLICY:
+                return "STATIC_BINDING";
+            case DYNAMIC_PRIORITY_BINDING_POLICY:
+                return "DYNAMIC_PRIORITY_BINDING";
+            default:
+                return "CUSTOMIZED"; // Optimistic!
+        }
+    }
+
+    private static String stateToString(int state) {
+        switch (state) {
+            case BROKEN:
+                return "BROKEN";
+            case UNRESOLVED:
+                return "UNRESOLVED";
+            case RESOLVED:
+                return "RESOLVED";
+            default:
+                return "unknown";
+        }
+    }
+
+    // WARN: This is a hack!
+    private static Dependency getDependency(DependencyDescription description) {
+        if (description == null) {
+            return null;
+        }
+        Field shunt = null;
+        try {
+            shunt = DependencyDescription.class.getDeclaredField("m_dependency");
+            shunt.setAccessible(true);
+            return (Dependency) shunt.get(description);
+        } catch (Exception e) {
+            throw new IllegalStateException("cannot get component instance", e);
+        } finally {
+            if (shunt != null) {
+                shunt.setAccessible(false);
+            }
+        }
     }
 }
