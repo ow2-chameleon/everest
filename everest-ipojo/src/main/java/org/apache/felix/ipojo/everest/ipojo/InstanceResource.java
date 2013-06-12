150a16620e96cea6e04b8d7364ef9d4166d21e1c
Pierre Bourret <Pierre.Bourret@imag.fr>
2013-06-12 16:29:52 +0200

    everest-ipojo: Fixed very bad bug in InstanceResource configuration retrieval + added configuration in fake instance resources

--- everest-ipojo/src/main/java/org/apache/felix/ipojo/everest/ipojo/InstanceResource.java
+++ everest-ipojo/src/main/java/org/apache/felix/ipojo/everest/ipojo/InstanceResource.java
@@ -4,6 +4,7 @@ import org.apache.felix.ipojo.ComponentInstance;
 import org.apache.felix.ipojo.Factory;
 import org.apache.felix.ipojo.InstanceStateListener;
 import org.apache.felix.ipojo.architecture.Architecture;
+import org.apache.felix.ipojo.architecture.InstanceDescription;
 import org.apache.felix.ipojo.architecture.PropertyDescription;
 import org.apache.felix.ipojo.everest.core.Everest;
 import org.apache.felix.ipojo.everest.impl.DefaultRelation;
@@ -123,6 +124,8 @@ public class InstanceResource extends ResourceMap implements InstanceStateListen
     public static Resource fakeInstanceResource(ComponentInstance instance) {
         String name = instance.getInstanceName();
         Factory factory = instance.getFactory();
+
+
         try {
             return new Builder()
                     .fromPath(INSTANCES.addElements(name))
@@ -131,6 +134,7 @@ public class InstanceResource extends ResourceMap implements InstanceStateListen
                             .set("factory.name", factory.getName())
                             .set("factory.version", factory.getVersion())
                             .set("state", stateAsString(instance.getState()))
+                            .set("configuration", getInstanceConfiguration(instance.getInstanceDescription())) // ResourceMetadata
                             .build())
                     .with(new DefaultRelation(
                             FACTORIES.addElements(factory.getName(), String.valueOf(factory.getVersion())),
@@ -153,21 +157,10 @@ public class InstanceResource extends ResourceMap implements InstanceStateListen
             return m;
         }
 
-        // Get the configuration
-        ImmutableResourceMetadata.Builder props = new ImmutableResourceMetadata.Builder();
-        ConfigurationHandlerDescription chd = (ConfigurationHandlerDescription)
-                i.getInstanceDescription().getHandlerDescription("org.apache.felix.ipojo:requires");
-        if (chd != null) {
-            PropertyDescription[] pp = chd.getProperties();
-            for (PropertyDescription p : pp) {
-                props.set(p.getName(), p.getValue());
-            }
-        }
-
         // Add dynamic metadata
         return new ImmutableResourceMetadata.Builder(m)
                 .set("state", stateAsString(i.getInstanceDescription().getState())) // String
-                .set("configuration", props) // Map
+                .set("configuration", getInstanceConfiguration(i.getInstanceDescription())) // ResourceMetadata
                 .build();
     }
 
@@ -238,5 +231,19 @@ public class InstanceResource extends ResourceMap implements InstanceStateListen
     public void stateChanged(ComponentInstance instance, int newState) {
         // Fire UPDATED event
         Everest.postResource(ResourceEvent.UPDATED, this);
+    }
+
+    private static ResourceMetadata getInstanceConfiguration(InstanceDescription instanceDescription) {
+        // Get the configuration
+        ImmutableResourceMetadata.Builder props = new ImmutableResourceMetadata.Builder();
+        ConfigurationHandlerDescription chd = (ConfigurationHandlerDescription)
+                instanceDescription.getHandlerDescription("org.apache.felix.ipojo:properties");
+        if (chd != null) {
+            PropertyDescription[] pp = chd.getProperties();
+            for (PropertyDescription p : pp) {
+                props.set(p.getName(), p.getValue());
+            }
+        }
+        return props.build();
     }
 }
